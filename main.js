0218c8287d3d81e66adbab6b59eb22b2e89da60e
leckster <leckie.gunter@byu.edu>
2014-04-02 16:08:18 -0600

    added main.js code

---
+++ main.js
@@ -0,0 +1,80 @@
+var MUSCLE_GROUPS = [
+	{
+		name: "Back",
+		exercises: [
+		{
+			name: ""
+		},
+		{
+			name: ""
+		}
+		]
+	},
+	{
+		name: "Arms",
+		exercises: [
+		{
+			name: ""
+		},
+		{
+			name: ""
+		}
+		]
+	},
+	{
+		name: "Core",
+		exercises: [
+		{
+			name: ""
+		},
+		{
+			name: ""
+		}
+		]
+	},
+	{
+		name: "Legs",
+		exercises: [
+		{
+			name: ""
+		},
+		{
+			name: ""
+		}
+		]
+	},
+	{
+		name: "Chest",
+		exercises: [
+		{
+			name: ""
+		},
+		{
+			name: ""
+		}
+		]
+	},
+	{
+		name: "Neck",
+		exercises: [
+		{
+			name: ""
+		},
+		{
+			name: ""
+		}
+		]
+	},
+	{
+		name: "Shoulders",
+		exercises: [
+		{
+			name: ""
+		},
+		{
+			name: ""
+		}
+		]
+	}
+];
+
