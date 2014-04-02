39c3cd99098731c097c1b68b70451abe8ca6e8fa
jarr416 <jarr416@gmail.com>
2014-04-02 16:22:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,89 @@ var MUSCLE_GROUPS = [
 		name: "Back",
 		exercises: [
 		{
-			name: ""
+			name: "Wide Grip Pull Down"
 		},
 		{
-			name: ""
+			name: "Seated Row Machine"
+		},
+		{
+			name: "One Arm Dumbell Rows"
+		},
+		{
+			name: "Bent Over Rows"
+		},
+		{
+			name: "One Arm Barbell Rows"
+		},
+		{
+			name: "Pull Ups"
+		},
+		{
+			name: "Two Arm Barbell Rows"
 		}
 		]
 	},
 	{
-		name: "Arms",
+		name: "Biceps",
 		exercises: [
 		{
-			name: ""
+			name: "Dumbell Hammer Curls"
 		},
 		{
-			name: ""
+			name: "Preacher Curls"
+		},
+		{
+			name: "Striaght Bar Curls"
+		},
+		{
+			name: "Dumbell Curls"
+		},
+		{
+			name: "Preacher Curl Machine"
+		},
+		{
+			name: "One Arm Pulley Curls"
+		},
+		{
+			name: "Two Arm Pulley Curls"
+		},
+		{
+			name: "Chin Ups"
+		},
+		{
+			name: "Concentration Curls"
+		}
+		]
+	},
+	{
+		name: "Triceps",
+		exercises: [
+		{
+			name: "Two Hand Overhead Extenstion"
+		},
+		{
+			name: "One Hand Overhead Extentsion"
+		},
+		{
+			name: "Dumbell Kickbacks"
+		},
+		{
+			name: "One Arm Pushdown"
+		},
+		{
+			name: "Two Arm Pushdown"
+		},
+		{
+			name: "Dips"
+		},
+		{
+			name: "Dip Machine"
+		},
+		{
+			name: "Tricep Machine"
+		},
+		{
+			name: "Skull Crushers"
 		}
 		]
 	},
@@ -25,21 +93,101 @@ var MUSCLE_GROUPS = [
 		name: "Core",
 		exercises: [
 		{
-			name: ""
+			name: "Ab Bridges"
 		},
 		{
-			name: ""
+			name: "Back Hyperextensions"
+		},
+		{
+			name: "Side Hyperextensions"
+		},
+		{
+			name: "Toe To Bar"
+		},
+		{
+			name: "Leg Raises"
+		},
+		{
+			name: "6 Inch Leg Lifts"
+		},
+		{
+			name: "Crunches"
+		},
+		{
+			name: "Sit Ups"
 		}
 		]
 	},
 	{
-		name: "Legs",
+		name: "Quadriceps",
 		exercises: [
 		{
-			name: ""
+			name: "Squats"
 		},
 		{
-			name: ""
+			name: "Deadlifts"
+		},
+		{
+			name: "Lunges"
+		},
+		{
+			name: "Smth Machine"
+		},
+		{
+			name: "Wall Sits"
+		},
+		{
+			name: "One Legged Leg Extension"
+		},
+		
+		{
+			name: "wo Legged Leg Extension"
+		}
+		]
+	},
+	{
+		name: "Hamstrings",
+		exercises: [
+		{
+			name: "One Legged Lying Hamstring Curl"
+		},
+		{
+			name: "Two Legged Lying Hamstring Curl"
+		},
+		{
+			name: "One Legged Sitting Hamstring Curl"
+		},
+		{
+			name: "Two Legged Sitting Hamstring Curl"
+		},
+		{
+			name: "Straight Leg Dead Lifts"
+		},
+		{
+			name: "Donkey Kicks on Hip Machine"
+		}
+		]
+	},
+	{
+		name: "Calf Raises",
+		exercises: [
+		{
+			name: "One Leg Standing Calf Raise"
+		},
+		{
+			name: "Two Leg Standing Calf Raises"
+		},
+		{
+			name: "Calf Raises on Leg Press Machine"
+		},
+		{
+			name: "Seated Calf Raise"
+		},
+		{
+			name: "One Legged Calf Raise Machine"
+		},
+		{
+			name: "Two Legged Calf Raise Machine"
 		}
 		]
 	},
@@ -51,12 +199,19 @@ var MUSCLE_GROUPS = [
 		},
 		{
 			name: ""
-		}
-		]
-	},
-	{
-		name: "Neck",
-		exercises: [
+		},
+		{
+			name: ""
+		},
+		{
+			name: ""
+		},
+		{
+			name: ""
+		},
+		{
+			name: ""
+		},
 		{
 			name: ""
 		},
@@ -76,5 +231,5 @@ var MUSCLE_GROUPS = [
 		}
 		]
 	}
-];
+};
 
